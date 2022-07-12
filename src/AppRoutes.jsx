30aa5c6d82a5b7167582d0a1f5884296fdae8039
JansenDF <jansen.lima.df@gmail.com>
2022-07-12 10:54:16 -0300

    Add PrivateRouter

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,18 +1,29 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React, { useState, useContext } from 'react'
+import { BrowserRouter, Routes, Route, PrivateRouter, Navigate } from 'react-router-dom';
 import { LoginPage } from './Pages/LoginPage';
 import { HomePage } from './Pages/HomePage';
-import { AuthProvider } from './context/auth';
+import { AuthProvider, AuthContext } from './context/auth';
 
 export const AppRoutes = () => {
 
+    const PrivateRouter = ( {children} ) => {
+
+        const  { authenticated } = useContext(AuthContext);
+
+        if(!authenticated){
+            return(
+                <Navigate to={"/login"} />
+            )
+        }
+        return children;
+    }
 
     return (
         <BrowserRouter>
             <AuthProvider>
                 <Routes>
                     <Route exact path='/login' element={<LoginPage />} />
-                    <Route exact path='/' element={<HomePage />} />
+                    <Route exact path='/' element={<PrivateRouter><HomePage /></PrivateRouter>} />
                 </Routes>
             </AuthProvider>
         </BrowserRouter>
